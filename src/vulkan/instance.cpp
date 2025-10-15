12a6c024320fca90a43a21a87795988fa45c48b5
Isaac Dobbins <isaac.dobbins@icloud.com>
2025-10-14 19:20:29 -0600

    working on instance

--- src/vulkan/instance.cpp
+++ src/vulkan/instance.cpp
@@ -14,13 +14,13 @@ using namespace std;
 constexpr string_view APP_NAME = "callandor";
 
 #if defined(__APPLE__)
-constexpr array<const char*, 3> instance_exts{
+constexpr array<const char*, 3> instanceExtensions{
     VK_KHR_SURFACE_EXTENSION_NAME,
     VK_EXT_METAL_SURFACE_EXTENSION_NAME,
     VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
   };
 #elif defined(_WIN32)
-constexpr array<const char*, 2> instance_exts{
+constexpr array<const char*, 2> instanceExtensions{
     VK_KHR_SURFACE_EXTENSION_NAME,
     VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
   };
@@ -35,39 +35,39 @@ constexpr array<const char*, 1> instance_exts{
   };
 #endif
 
-static std::vector<VkExtensionProperties> exts() {
+static vector<VkExtensionProperties> enumerateInstanceExtensionProperties() {
     uint32_t count = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &count, nullptr);
 
-    std::vector<VkExtensionProperties> exts(count);
+    vector<VkExtensionProperties> exts(count);
 
     vkEnumerateInstanceExtensionProperties(nullptr, &count, exts.data());
 
     return exts;
 }
 
-VkInstance create_instance()
+static VkInstance createInstance()
 {
-    const VkApplicationInfo app_info{
+    static constexpr VkApplicationInfo app_info {
         .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
         .pApplicationName = APP_NAME.data(),
         .pEngineName = APP_NAME.data(),
         .apiVersion = VK_API_VERSION_1_3,
-      };
+    };
 
-    VkInstanceCreateInfo ici{
+    static constexpr VkInstanceCreateInfo create_info {
         .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-        .pApplicationInfo = &app_info,
-        .enabledExtensionCount = static_cast<u32>(instance_exts.size()),
-        .ppEnabledExtensionNames = instance_exts.data(),
-      };
-
 #if defined(__APPLE__)
-    ici.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+        .flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR,
 #endif
+        .pApplicationInfo = &app_info,
+        .enabledExtensionCount = static_cast<u32>(instanceExtensions.size()),
+        .ppEnabledExtensionNames = instanceExtensions.data(),
+    };
 
     VkInstance inst = VK_NULL_HANDLE;
-    runtime_assert(vkCreateInstance(&ici, nullptr, &inst) == VK_SUCCESS, "Failed to create instance");
+    VkResult r = vkCreateInstance(&create_info, nullptr, &inst);
+    runtime_assert(r == VK_SUCCESS, "Failed to create instance");
     return inst;
 }
 
