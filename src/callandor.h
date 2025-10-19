e6fab305d73cf0c36bab7c7dca67f813e12c05c4
Isaac Dobbins <isaac.dobbins@icloud.com>
2025-10-19 16:13:28 -0600

    we are working on the design of this application

---
+++ src/callandor.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <span>
+
+//------------------------------------------------------------------------------------
+// Forward Declarations
+//------------------------------------------------------------------------------------
+
+template<typename T>struct Config;
+
+struct Window;
+
+// Vulkan (c library) forward declarations
+struct VkInstance_T;
+typedef VkInstance_T* VkInstance;
+
+struct VkSurfaceKHR_T;
+typedef VkSurfaceKHR_T *VkSurfaceKHR;
+
+struct VkPhysicalDevice_T;
+typedef VkPhysicalDevice_T* VkPhysicalDevice;
+
+struct Vulkan;
+struct InstanceConfig;
+
+//------------------------------------------------------------------------------------
+// Window Functions (Module: window)
+//------------------------------------------------------------------------------------
+
+Window Create(const Config<Window> &config);
+void Destroy(Window &window);
+bool IsDone(Window &window);
+bool IsReady(Window &window);
+
+//------------------------------------------------------------------------------------
+// Vulkan Functions (Module: vulkan)
+//------------------------------------------------------------------------------------
+
+void Create(Vulkan &v);
+void Destroy(Vulkan &v);
+
+VkInstance Create(const Config<VkInstance> &config);
+void Destroy(VkInstance &instance);
+
+VkSurfaceKHR Create(const Config<VkSurfaceKHR> &config);
+void Destroy(VkSurfaceKHR &surface);
+
+template<typename T>
+std::span<T> Enumerate();
+// Enumerate VkPhysicalDevice
+// Enumerate VkQueueFamilyProperties for a given VkPhysicalDevice
+
+// Shader management functions
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <callandor.h>
+
+int main()
+{
+    return 0;
+}
